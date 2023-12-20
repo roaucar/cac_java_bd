c1e2275db6e259136b811e57edfd8e39593cbfa2
roaucar <132701908+roaucar@users.noreply.github.com>
2023-12-19 21:55:19 -0300

    Update output.js

--- js/output.js
+++ js/output.js
@@ -21,26 +21,26 @@ public class TestBD {
             List<String> htmlOradores = new ArrayList<>();
 
             while (resultado.next()) {
-                String nombreOrador = resultado.getString("nombre");
-                String temaOrador = resultado.getString("tema");
-                String textoOrador = resultado.getString("texto");
-                String htmlOrador = "<div class=\"col-xxl-4 col-xl-4 col-md-4 col-sm-12 col-xs-12\">\n"
+                String nombreOradores = resultado.getString("nombre");
+                String temaOradores = resultado.getString("tema");
+                String textoOradores = resultado.getString("texto");
+                String htmlOradores = "<div class=\"col-xxl-4 col-xl-4 col-md-4 col-sm-12 col-xs-12\">\n"
                         + "    <div class=\"card\">\n"
                         + "        <img src=\"./assets/img/" + resultado.getString("imagen") + "\" class=\"card-img-top\" alt=\"" + nombreOrador + "\">\n"
                         + "        <div class=\"card-body\">\n"
-                        + "            <h5 class=\"java badge bg-warning text-dark\">" + temaOrador + "</h5>\n"
-                        + "            <h3 class=\"card-title\">" + nombreOrador + "</h3>\n"
-                        + "            <p class=\"card-text lh-lg pb-2\">" + textoOrador + "</p>\n"
+                        + "            <h5 class=\"java badge bg-warning text-dark\">" + temaOradores + "</h5>\n"
+                        + "            <h3 class=\"card-title\">" + nombreOradores + "</h3>\n"
+                        + "            <p class=\"card-text lh-lg pb-2\">" + textoOradores + "</p>\n"
                         + "        </div>\n"
                         + "    </div>\n"
                         + "</div>";
 
-                htmlOradores.add(htmlOrador);
+                htmlOradores.add(htmlOradores);
             }
 
             System.out.println("var oradoresHtml = [");
-            for (String htmlOrador : htmlOradores) {
-                System.out.println("    '" + htmlOrador.replace("'", "\\'") + "',");
+            for (String htmlOradores : htmlOradores) {
+                System.out.println("    '" + htmlOradores.replace("'", "\\'") + "',");
             }
             System.out.println("];");
 
